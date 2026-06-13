19339fd132cae6659cec498e3b2512b8793000f6
hemarosh7-code <hemarosh7@gmail.com>
2026-06-14 03:17:17 +0530

    changes in env

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({ 
-  baseURL: process.env.REACT_APP_API_URL 
-    ? `${process.env.REACT_APP_API_URL}/api` 
-    : 'http://localhost:5000/api' 
+  baseURL: 'https://patient-registration-wod4.onrender.com/api'
 });
 
 export const addPatient    = (data)                             => API.post('/patients', data);
