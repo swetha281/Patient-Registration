80bfbecabe56fa9d16186e204f396511d52dae92
hemarosh7-code <hemarosh7@gmail.com>
2026-06-14 02:37:06 +0530

    changes in frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,13 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+const API = axios.create({ 
+  baseURL: process.env.REACT_APP_API_URL 
+    ? `${process.env.REACT_APP_API_URL}/api` 
+    : 'http://localhost:5000/api' 
+});
 
-export const addPatient    = (data)                            => API.post('/patients', data);
+export const addPatient    = (data)                             => API.post('/patients', data);
 export const getPatients   = (search = '', page = 1, limit = 5) => API.get(`/patients?search=${search}&page=${page}&limit=${limit}`);
-export const getPatient    = (id)                              => API.get(`/patients/${id}`);
-export const updatePatient = (id, data)                        => API.put(`/patients/${id}`, data);
-export const deletePatient = (id)                              => API.delete(`/patients/${id}`);
+export const getPatient    = (id)                               => API.get(`/patients/${id}`);
+export const updatePatient = (id, data)                         => API.put(`/patients/${id}`, data);
+export const deletePatient = (id)                               => API.delete(`/patients/${id}`);
